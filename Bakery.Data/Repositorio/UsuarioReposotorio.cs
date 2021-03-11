533631c7b58df994c853429476df31f77ca831d5
Renato Salgado Dias <renaato.salgado@gmail.com>
2021-03-11 20:52:12 -0300

    Criando usuário

---
+++ Bakery.Data/Repositorio/UsuarioReposotorio.cs
@@ -0,0 +1,17 @@
+﻿using Bakery.Data.Interface;
+using Bakery.Data.Repository;
+using Bakery.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bakery.Data.Repositorio
+{
+    public class UsuarioReposotorio : BaseRepositorio<Usuario>, IUsuarioRepositorio
+    {
+        public UsuarioReposotorio(Contexto contexto) : base(contexto)
+        {
+
+        }
+    }
+}
---
+++ Bakery/Controllers/UsuarioController.cs
@@ -0,0 +1,49 @@
+﻿using Bakery.Data.Interface;
+using Bakery.Dominio;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Bakery.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+
+        public UsuarioController(IUsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+        // GET api/<UsuarioController>/5
+        [HttpGet("{id}")]
+        public IActionResult<Usuario> Get(int id)
+        {
+            return _usuarioRepositorio.Selecionar(id);
+        }
+
+        // POST api/<UsuarioController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UsuarioController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsuarioController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
