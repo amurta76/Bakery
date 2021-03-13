13355ae34a52c742a777c0de7ee772560b6348ad
Renato Salgado Dias <renaato.salgado@gmail.com>
2021-03-13 18:13:18 -0300

    Mudanças

--- Bakery/Controllers/EstoqueController.cs
+++ Bakery/Controllers/EstoqueController.cs
@@ -68,7 +68,6 @@ namespace Bakery.Controllers
             }
             catch (Exception)
             {
-
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
 
