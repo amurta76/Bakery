e9f76189d6fdcc60b2fc8fcd02a95e92cfb7b582
Valeska Oliveira <valeskaboliveira@gmail.com>
2021-03-16 17:22:01 -0300

    Caixa Repositório

--- Bakery.Data/Repositorio/CaixaRepositorio.cs
+++ Bakery.Data/Repositorio/CaixaRepositorio.cs
@@ -12,6 +12,13 @@ namespace Bakery.Data.Repositorio
     {
         public CaixaRepositorio(Contexto contexto) : base(contexto) { }
 
-        
+        var contagem = _contexto.Set<Caixa>()
+                            .Where(ca => ca.IdIdCaixa == IdCaixa &&
+                                        ca.ProdutoFinalProduzido.Situacao == true).Count();
+
+            if (contagem > 0)
+                return false;
+            else
+                return true;
     }
 }
