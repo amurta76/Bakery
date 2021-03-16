9b08486ae9129dc7924c7ce19a9bae5d4e595093
Valeska Oliveira <valeskaboliveira@gmail.com>
2021-03-16 18:11:50 -0300

    Situação Caixa

--- Bakery.Data/Repositorio/CaixaRepositorio.cs
+++ Bakery.Data/Repositorio/CaixaRepositorio.cs
@@ -15,15 +15,14 @@ namespace Bakery.Data.Repositorio
 
         public bool VerificaExistenciaDeCaixaEmAberto()
         {
-            //var contagem = _contexto.Set<Caixa>()
-            //                .Where(ca => ca.Id == IdCaixa &&
-            //                            ca.ProdutoFinalProduzido.Situacao == true).Count();
+            var contagem = _contexto.Set<Caixa>()                              
+                     .Where(ca => ca.Situacao == true).Count();
 
-            //if (contagem > 0)
-            //    return false;
-            //else
-            //    return true;
-            return true
+            if (contagem > 0)
+                return false;
+            else
+                return true;
+          
         }
     }
 }
